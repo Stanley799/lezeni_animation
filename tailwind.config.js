a9fb4758aef6ffbb70fc834bfe8cd8dc2e35dd4f
Stanley799 <oduor.otieno@strathmore.edu>
2025-09-19 14:06:28 +0300

    issues with postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors: { primary: '#1f2937' /* dark theme */ },
+      colors: { primary: '#1f2937' },
     },
   },
   plugins: [],
